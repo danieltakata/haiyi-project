22d0b68182d378216491daf2124a186d69c7208d
Terrance <grayx501@umn.edu>
2018-08-01 13:37:32 -0500

    Updated features of white interactive w/ mystery, etc.

--- WhiteInteractive/js/scripts.js
+++ WhiteInteractive/js/scripts.js
@@ -44,6 +44,10 @@ google.charts.setOnLoadCallback(function() {
     greWriWt = 0.3124308781,
     gpaWt = 1.456150174,
     rankWt = 0.003788262564,
+    // Institution Rank weights
+    tier1Wt = 0.5,
+    tier2Wt = 0.30693782874,
+    tier3Wt = 0,
     // major weights
     csceWt = 1.5594831748,
     stemWt = 0.30693782874,
@@ -57,18 +61,15 @@ google.charts.setOnLoadCallback(function() {
     psWt = 0.8300898821,
     diverWt = 0.4184694261,
     // recommendation weights
-    rec1top5Wt = 2.00627220247,
-    rec1top10Wt = 1.11329686474,
-    rec1top20Wt = 0.87623872879,
-    rec1top50Wt = 0,
-    rec2top5Wt = 1.52087808051,
-    rec2top10Wt = 1.00101344554,
-    rec2top20Wt = 0.44907150816,
-    rec2top50Wt = 0,
-    rec3top5Wt = 1.51416715224,
-    rec3top10Wt = 0.93864978431,
-    rec3top20Wt = 0.27703027917,
-    rec3top50Wt = 0,
+    rec1strong = 2.00627220247,
+    rec1average = 1.11329686474,
+    rec1weak = 0,
+    rec2strong = 2.00627220247,
+    rec2average = 1.11329686474,
+    rec2weak = 0,
+    rec3strong = 2.00627220247,
+    rec3average = 1.11329686474,
+    rec3weak = 0
 
     rec1instWt = -0.006982607425,
     rec2instWt = -1.58e-03,
@@ -85,54 +86,57 @@ google.charts.setOnLoadCallback(function() {
     psVal = 3,
     diverVal = 3;
 
-  var weights = [
-    0.114911184251 // GRE-verb
-    , 0.0880320310069 // GRE-quant
-    , 0.2926697271 // GRE-write
-    , 1.584509831 // GPA
-    , 0.00294640714041 // Inst-Rank
-    , 1 // Major
-    , 1 // Country of origin
-    , 1 // Recommendation letter 1
-    , 0.00643092126893 // Rec1 inst
-    , 1 // Recommendation letter 2
-    , 0.00163152813703 // Rec2 inst
-    , 1 // Recommendation letter 3
-    , 0.00109465199226 // Rec3 inst
-    , 0.756277254266 // Personal Statement
-    , 0.317935719456 // Diversity score
-  ];
-  // inital values
-  var vals = [
-    12 // GRE-verb - 130
-    , 10 // GRE-quant - 130
-    , 3 // GRE-write
-    , 3.2 // GPA
-    , 274 // 1000 - Inst-Rank
-    , 1.5594831748 // Major
-    , 0 // Country of origin
-    , 0.87623872879 // Recommendation letter 1
-    , 0 // 1000 - Rec1 inst rank
-    , 0.44907150816 // Recommendation letter 2
-    , 0 // 1000 - Rec2 inst rank
-    , 0.27703027917 // Recommendation letter 3
-    , 0 // 1000 - Rec3 inst rank
-    , 2 // Personal Statement - 1
-    , 1 // Diversity score - 1
-  ];
-
-
-  var studentData = [
-    ['Total Score', 'GRE-verb', 'GRE-quant', 'GRE-write', 'GPA', 'Inst-Rank', 'Major', 'Country', 'Rec1', 'Rec1 Rank', 'Rec2', 'Rec2 Rank', 'Rec3', 'Rec3 Rank', 'PS', 'Diversity'],
-    ['', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-  ];
-  for (var i = 0; i < studentData[1].length - 1; i++) {
-    studentData[1][i + 1] = vals[i] * weights[i];
-  }
-
-
-  var threshold = 42.962524 - 130 * weights[0] - 130 * weights[1] + 0.622140334514 + 0.805488066489 + 0.742740758554 + 0.875925686442 + 0.513354407198 - weights[13] - weights[14] //threshold needs to minus weight of PS/ diversity?
-
+    var weights = [
+      0.114911184251 // GRE-verb
+      , 0.0880320310069 // GRE-quant
+      , 0.2926697271 // GRE-write
+      , 1.584509831 // GPA
+      , 1 // Inst-Rank
+      , 1 // Major
+      , 1 // Country of origin
+      , 1 // Recommendation letter 1
+      , 1 // Recommendation letter 2
+      , 1 // Recommendation letter 3
+      , 0.03643092126893 // Mystery 1
+      , -0.0363152813703 // Mystery 2
+      , 0.000109465199226 // Mystery 3
+      , 0.756277254266 // Personal Statement
+      , 0.317935719456 // Diversity score
+    ];
+    // inital values
+    var vals = [
+      12             // GRE-verb - 130
+      ,10            // GRE-quant - 130
+      ,3             // GRE-write
+      ,3.2           // GPA
+      ,1           // 1000 - Inst-Rank
+      ,1.5594831748  // Major
+      ,0             // Country of origin
+      ,0.87623872879 // Recommendation letter 1
+      ,0.44907150816 // Recommendation letter 2
+      ,0.27703027917 // Recommendation letter 3
+      ,50             // Mystery 1
+      ,50             // Mystery 2
+      ,50             // Mystery 3
+      ,2             // Personal Statement - 1
+      ,1             // Diversity score - 1
+    ];
+  
+  
+    var studentData = [
+      ['Total Score', 'GRE-verb', 'GRE-quant', 'GRE-write', 'GPA', 'Inst-Rank', 'Major', 'Country', 'Rec1', 'Rec2', 'Rec3', 'Mystery1', 'Mystery2', 'Mystery3', 'PS', 'Diversity'],
+      ['', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ];
+    for (var i = 0; i < studentData[1].length-1; i++) {
+      studentData[1][i + 1] = vals[i] * weights[i];
+    }
+  
+    // Original threshold
+    var original_threshold = 42.962524 - 130 * weights[0] - 130 * weights[1] + 0.622140334514 + 0.805488066489 + 0.742740758554 + 0.875925686442 + 0.513354407198 - weights[13] - weights[14] //threshold needs to minus weight of PS/ diversity?
+  
+    var low_threshold = 37.962524 - 130 * weights[0] - 130 * weights[1] + 0.622140334514 + 0.805488066489 + 0.742740758554 + 0.875925686442 + 0.513354407198 - weights[13] - weights[14] //threshold needs to minus weight of PS/ diversity?
+    var high_threshold = 47.962524 - 130 * weights[0] - 130 * weights[1] + 0.622140334514 + 0.805488066489 + 0.742740758554 + 0.875925686442 + 0.513354407198 - weights[13] - weights[14] //threshold needs to minus weight of PS/ diversity?
+  
   // var barcolors = ['#F01010', '#FB0074', '#C82CCC', '#006DFF', '#0089FF', '#90A0E0', '#00BF70', '#00BF70', '#009F00', '#009F00', '#5BE500', '#5BE500', '#FFA500', '#E05050', '#FF90B0']
 
   var barcolors = [
@@ -144,11 +148,11 @@ google.charts.setOnLoadCallback(function() {
     , '#f7a55d' // Major
     , '#B99483' // Country of origin
     , '#5ca053' // Recommendation letter 1
-    , '#74b869' // Rec1 inst
     , '#4d9794' // Recommendation letter 2
-    , '#6baaa5' // Rec2 inst
     , '#5079a5' // Recommendation letter 3
-    , '#77a2c6' // Rec3 inst
+    , '#74b869' // mystery1
+    , '#6baaa5' // mystery2
+    , '#77a2c6' // mystery3
     , '#df585c' // Personal Statement
     , '#fd9d9b' // Diversity score
   ];
@@ -271,27 +275,25 @@ google.charts.setOnLoadCallback(function() {
   // });
   // $("#rank-input").val(1000 - vals[4])
 
-  $("#rank-slider").slider({
-    // options
-    range: "min",
-    value: vals[4],
-    min: 0,
-    max: 999,
-    step: 1,
-    start: function(event, ui) {
-      // code
-    },
-    slide: function(event, ui) {
-      $("#rank-data").text((1000 - ui.value))
-    },
+  $("#institution-dropdown").selectmenu({
     change: function(event, ui) {
-      studentData[1][5] = ui.value * weights[4];
-      //      adjustedData[4] = (ui.value-studentData[2][5])*rankWt;
+      let institutionWt = 0;
+      switch ($("#institution-dropdown").val()) {
+        case 'tier1':
+          institutionWt = tier1Wt;
+          break;
+        case 'tier2':
+          institutionWt = tier2Wt;
+          break;
+        case 'tier3':
+          institutionWt = tier3Wt;
+          break;
+      }
+      studentData[1][5] = institutionWt * weights[4];
       drawStacked();
       updateResult();
     }
   });
-  $("#rank-slider .ui-slider-range").css('background', barcolors[4]);
 
 
   $("#major-dropdown").selectmenu({
@@ -344,17 +346,14 @@ google.charts.setOnLoadCallback(function() {
     change: function(event, ui) {
       let rec1Wt = 0;
       switch ($("#rec1-dropdown").val()) {
-        case 'rec1top5Wt':
-          rec1Wt = rec1top5Wt;
+        case 'rec1strong':
+          rec1Wt = rec1strong;
           break;
-        case 'rec1top10Wt':
-          rec1Wt = rec1top10Wt;
+        case 'rec1average':
+          rec1Wt = rec1average;
           break;
-        case 'rec1top20Wt':
-          rec1Wt = rec1top20Wt;
-          break;
-        case 'rec1top50Wt':
-          rec1Wt = rec1top50Wt;
+        case 'rec1weak':
+          rec1Wt = rec1weak;
           break;
       }
       studentData[1][8] = rec1Wt * weights[7];
@@ -367,20 +366,17 @@ google.charts.setOnLoadCallback(function() {
     change: function(event, ui) {
       let rec2Wt = 0;
       switch ($("#rec2-dropdown").val()) {
-        case 'rec2top5Wt':
-          rec2Wt = rec2top5Wt;
+        case 'rec2strong':
+          rec2Wt = rec2strong;
           break;
-        case 'rec2top10Wt':
-          rec2Wt = rec2top10Wt;
+        case 'rec2average':
+          rec2Wt = rec2average;
           break;
-        case 'rec2top20Wt':
-          rec2Wt = rec2top20Wt;
-          break;
-        case 'rec2top50Wt':
-          rec2Wt = rec2top50Wt;
+        case 'rec2weak':
+          rec2Wt = rec2weak;
           break;
       }
-      studentData[1][10] = rec2Wt * weights[9];
+      studentData[1][9] = rec2Wt * weights[8];
       drawStacked();
       updateResult();
     }
@@ -390,20 +386,18 @@ google.charts.setOnLoadCallback(function() {
     change: function(event, ui) {
       let rec3Wt = 0;
       switch ($("#rec3-dropdown").val()) {
-        case 'rec3top5Wt':
-          rec3Wt = rec3top5Wt;
+        case 'rec3strong':
+          rec3Wt = rec3strong;
           break;
-        case 'rec3top10Wt':
-          rec3Wt = rec3top10Wt;
+        case 'rec3average':
+          rec3Wt = rec3average;
           break;
-        case 'rec3top20Wt':
-          rec3Wt = rec3top20Wt;
-          break;
-        case 'rec3top50Wt':
-          rec3Wt = rec3top50Wt;
+        case 'rec3weak':
+          rec3Wt = rec3weak;
           break;
       }
-      studentData[1][12] = rec3Wt * weights[11];
+
+      studentData[1][10] = rec3Wt * weights[9];
       drawStacked();
       updateResult();
     }
@@ -415,46 +409,18 @@ google.charts.setOnLoadCallback(function() {
   // });
   // $("#rec1rank-input").val(1000 - vals[8]);
 
-  $("#rec1rank-slider").slider({
+  $("#mystery1-slider").slider({
     // options
     start: function(event, ui) {
       // code
     },
     slide: function(event, ui) {
-      $("#rec1rank-data").text((1000 - ui.value))
+      $("#mystery1-data").text((ui.value))
     },
     range: "min",
     min: 0,
-    max: 999,
-    value: vals[8],
-    step: 1,
-    change: function(event, ui) {
-      studentData[1][9] = ui.value * weights[8];
-      //    adjustedData[6] = (ui.value-studentData[2][7])*recWt
-      drawStacked();
-      updateResult();
-    }
-  });
-  $("#rec1rank-slider .ui-slider-range").css('background', barcolors[7]);
-
-  // $("#rec2rank-input").change(function() {
-  //   $("#rec2rank-slider").slider("value", 1000 - $("#rec2rank-input").val());
-  //   updateResult();
-  // });
-  // $("#rec2rank-input").val(1000 - vals[10]);
-
-  $("#rec2rank-slider").slider({
-    // options
-    start: function(event, ui) {
-      // code
-    },
-    slide: function(event, ui) {
-      $("#rec2rank-data").text((1000 - ui.value))
-    },
-    range: "min",
-    min: 0,
-    max: 999,
-    value: vals[10],
+    max: 100,
+    value: vals[11],
     step: 1,
     change: function(event, ui) {
       studentData[1][11] = ui.value * weights[10];
@@ -463,7 +429,35 @@ google.charts.setOnLoadCallback(function() {
       updateResult();
     }
   });
-  $("#rec2rank-slider .ui-slider-range").css('background', barcolors[9]);
+  $("#mystery1-slider .ui-slider-range").css('background', barcolors[7]);
+
+  // $("#rec2rank-input").change(function() {
+  //   $("#rec2rank-slider").slider("value", 1000 - $("#rec2rank-input").val());
+  //   updateResult();
+  // });
+  // $("#rec2rank-input").val(1000 - vals[10]);
+
+  $("#mystery2-slider").slider({
+    // options
+    start: function(event, ui) {
+      // code
+    },
+    slide: function(event, ui) {
+      $("#mystery2-data").text((ui.value))
+    },
+    range: "min",
+    min: 0,
+    max: 100,
+    value: vals[11],
+    step: 1,
+    change: function(event, ui) {
+      studentData[1][12] = ui.value * weights[11];
+      //    adjustedData[6] = (ui.value-studentData[2][7])*recWt
+      drawStacked();
+      updateResult();
+    }
+  });
+  $("#mystery2-slider .ui-slider-range").css('background', barcolors[9]);
 
 
 
@@ -473,17 +467,17 @@ google.charts.setOnLoadCallback(function() {
   // });
   // $("#rec3rank-input").val(1000 - vals[12]);
 
-  $("#rec3rank-slider").slider({
+  $("#mystery3-slider").slider({
     // options
     start: function(event, ui) {
       // code
     },
     slide: function(event, ui) {
-      $("#rec3rank-data").text((1000 - ui.value))
+      $("#mystery3-data").text((ui.value))
     },
     range: "min",
     min: 0,
-    max: 999,
+    max: 100,
     value: vals[12],
     step: 1,
     change: function(event, ui) {
@@ -492,7 +486,7 @@ google.charts.setOnLoadCallback(function() {
       updateResult();
     }
   });
-  $("#rec3rank-slider .ui-slider-range").css('background', barcolors[11]);
+  $("#mystery3-slider .ui-slider-range").css('background', barcolors[11]);
 
   // $("#ps-input").change(function() {
   //   $("#ps-slider").slider("value", $("#ps-input").val() - 1);
@@ -553,32 +547,38 @@ google.charts.setOnLoadCallback(function() {
 
 
   function updateResult() {
-    var totalScore = studentData[1][1] + studentData[1][2] + studentData[1][3] + studentData[1][4] + studentData[1][5] +
-      studentData[1][6] + studentData[1][7] + studentData[1][8] + studentData[1][9] + studentData[1][10] +
-      studentData[1][11] + studentData[1][12] + studentData[1][13] + studentData[1][14];
-
-    console.log(totalScore);
+    // var totalScore = studentData[1][1] + studentData[1][2] + studentData[1][3] + studentData[1][4] + studentData[1][5] + studentData[1][6] + studentData[1][7] + studentData[1][8] + studentData[1][9] + studentData[1][10] + studentData[1][11];
+    var totalScore = 0;
+    for (var i = 1; i < studentData[1].length; i++) {
+      totalScore += studentData[1][i];
+    }
+    // console.log(totalScore);
 
     // Loading animation
-    $(".result").css('opacity', 1).animate({
+    $("#result").css('opacity', 1).animate({
       opacity: 0
     }, 0.1, function() {
       $(".loader").css('visibility', 'visible');
       setTimeout(function() {
         $(".loader").css('visibility', 'hidden');
-        if (totalScore >= threshold) {
-          $("#result").text("Admission accepted.");
+        if (totalScore >= high_threshold) {
+          $("#result").text("Likely accepted");
           $("#result").css("color", "green");
+        } else if (totalScore >= original_threshold && totalScore < high_threshold){
+          $("#result").text("Probably accepted");
+          $("#result").css("color", "LimeGreen ");
+        } else if (totalScore >= low_threshold && totalScore < original_threshold){
+          $("#result").text("Probably rejected");
+          $("#result").css("color", "crimson ");
         } else {
-          $("#result").text("Admission rejected.");
+          $("#result").text("Likely rejected");
           $("#result").css("color", "red");
         }
-        $(".result").css('opacity', 0).animate({
+        $("#result").css('opacity', 0).animate({
           opacity: 1
         }, 200);
       }, 500);
     });
-
 
   }
 
@@ -622,7 +622,7 @@ google.charts.setOnLoadCallback(function() {
           color: "black",
         },
         ticks: [{
-          v: threshold,
+          v: original_threshold,
           f: "Acceptance Threshold"
         }, {
           v: 40.236724062345,
